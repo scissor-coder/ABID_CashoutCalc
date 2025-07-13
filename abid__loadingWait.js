f6592dafd915ff1a85153ed8750a178e94301405
scissor-coder <alhasan161177@outlook.com>
2025-07-13 19:39:20 +0600

    Add files via upload

---
+++ abid__loadingWait.js
@@ -0,0 +1,14 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+
+    //delete the loader from html code
+    document.querySelector(".loader").addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
